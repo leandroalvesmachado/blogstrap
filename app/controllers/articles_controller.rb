b42e88db9c3b8cae7efde33b9203c7e6e3e9f5fe
Leandro Machado <leandroalvesmachado@gmail.com>
2022-04-26 14:59:12 -0300

    Aula 09 - Adicionando Bootstrap 5 - finalizada

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,13 @@
 class ArticlesController < ApplicationController
+  # before_action :set_article, only: [:show, :edit, :update, :destroy]
+  # versão reduzida
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,12 +15,39 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params)
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
